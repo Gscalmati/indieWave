046d2940102ffd26a68e80e2aa59a659d7e4fce0
Gscalmati <gioscalmati@hotmail.com>
2021-09-19 22:45:24 -0300

    Creacion App.js y HTML

---
+++ indieWave/app.js
@@ -0,0 +1,15 @@
+
+const express = require('express');
+const app = express();
+
+const path = require("path");
+
+app.use(express.static(path.resolve(__dirname,"public")));
+
+app.listen(3000, () => {
+    console.log("Running on 3000")
+})
+
+app.get("/", (req,res) =>{
+    res.sendFile(path.resolve(__dirname, "views/index.html"));
+})
