59a700efbf7aee3b469110d25e283d92fb558e44
Facundo-B <fmblanco97@gmail.com>
2021-10-22 00:05:33 -0300

    Termino lógica vista categoryGames

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -9,66 +9,83 @@ let jsonProducts = fs.readFileSync(path.resolve(__dirname, '../data/products.jso
 let products = JSON.parse(jsonProducts); //Convertimos el json a array
 
 /* Función para conseguir el nuevo ID */
-const newId = function(){
+const newId = function () {
     let idNum = 0;
     products.forEach(product => {
         if (product.id > idNum) {
-            idNum=product.id
+            idNum = product.id
         }
     })
-    return idNum+1;
+    return idNum + 1;
 }
 
 console.log(newId());
 
 
 productsController = {
-    categories: function (req, res){
+    categories: function (req, res) {
         /*Creo un array por cada género*/
-        let arcade = products.filter((game)=>game.genre == "arcade")
-        let action = products.filter((game)=>game.genre == "action")
-        let sports = products.filter((game)=>game.genre == "sports")
-        let strategy = products.filter((game)=>game.genre == "strategy")
-        let adventure = products.filter((game)=>game.genre == "adventure")
-        
-        res.render("products/categories", {arcade, action, sports, strategy, adventure}); 
+        let arcade = products.filter((game) => game.genre == "arcade")
+        let action = products.filter((game) => game.genre == "action")
+        let sports = products.filter((game) => game.genre == "sports")
+        let strategy = products.filter((game) => game.genre == "strategy")
+        let adventure = products.filter((game) => game.genre == "adventure")
+
+        res.render("products/categories", { arcade, action, sports, strategy, adventure });
     },
-    categorygames: function (req, res){
+    categorygames: function (req, res) {
         let category = req.params.category;
-
-        /* Me guarto el array de esa categoria 
         
-        let categoryGames = json[category]
-        
-        */
+        let categoryGames = products.filter((game) => game.genre == category)
 
-        res.render("products/categoryGames", /*el array con los juegos de esa categoria*/); 
+        let title = ""
+        switch (category) {
+            case "arcade":
+                title = "Arcade";
+                break;
+            case "action":
+                title = "Acción"
+                break;
+            case "strategy":
+                title = "Estrategia"
+                break;
+            case "adventure":
+                title = "Aventura"
+                break;
+            case "sports":
+                title = "Deportes"
+                break;
+
+        }
+
+
+        res.render("products/categoryGames", { categoryGames, category, title });
     },
 
-    detail: function (req, res){
-        let detallado = products.filter(product =>{
+    detail: function (req, res) {
+        let detallado = products.filter(product => {
             return (product.id == req.params.id);
-        }) 
-        
-        res.render("products/productDetail", {detallado: detallado[0]});
+        })
+
+        res.render("products/productDetail", { detallado: detallado[0] });
         /* No imprime en el EJS */
     },
 
-    dashboard: function(req, res){
+    dashboard: function (req, res) {
         res.render("products/dashboard")
     },
 
-    cart: function(req, res){
+    cart: function (req, res) {
         res.render("products/shoppingCart");
     },
-    edit: function(req, res){
+    edit: function (req, res) {
         res.render("products/productEdit");
     },
-    create: function(req, res){
+    create: function (req, res) {
         res.render("products/productCreate")
     },
 
-    store: function(req, res){
+    store: function (req, res) {
         let newProduct = {
             id: newId(),
             game_name: req.body.productName,
@@ -102,7 +119,7 @@ productsController = {
         res.redirect("/products");
     },
 
-    editArt: function(req, res){
+    editArt: function (req, res) {
         res.send("Artículo editado")
     }
 }
