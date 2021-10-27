9ce57a9dfd88f94a7926ed9c93edce620281c795
Gscalmati <gioscalmati@hotmail.com>
2021-10-26 23:15:34 -0300

    Implementacion de Multer

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,10 +1,43 @@
 const express = require("express");
 const router = express.Router();
+const path = require("path");
+const fs = require('fs');
+
+/* Configuración de Multer  */
+const multer = require("multer");
+const storage = multer.diskStorage({
+    destination : (req, file, cb) =>{
+        console.log(req.body);
+        if (!fs.existsSync(path.resolve(__dirname, `../../public/img/${req.body.productName}-imgs`))){
+            fs.mkdirSync(path.resolve(__dirname, `../../public/img/${req.body.productName}-imgs`));
+        }
+        cb(null, path.resolve(__dirname, `../../public/img/${req.body.productName}-imgs`));
+    },
+    filename: (req, file, cb) => {
+        let newFilename
+        if (file.fieldname === "logo"){
+            newFilename = "Logo" + req.body.productName + Date.now() + path.extname(file.originalname)
+        } else{
+            newFilename = "Img"+ req.body.productName + Date.now() + path.extname(file.originalname)
+        }
+        cb(null, newFilename )
+    }
+})
+const upload = multer({storage});
 
 const productsController = require("../controllers/productsController");
 
+/* Middlewares req.body=vacio????*/
+function makeDirectory (req, res, next){
+    console.log(req.body)
+    fs.mkdirSync(path.resolve(__dirname, `../../public/img/${req.body.productName}-imgs`));
+    next();
+}
+
+
+
 router.get("/", productsController.categories);
-router.post("/", productsController.store);
+router.post("/", upload.fields([{name: "logo"},{name: "image"}]), productsController.store);
 
 /* CRUD */
 router.get("/dashboard", productsController.dashboard);
