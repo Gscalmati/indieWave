187df873d7634c90535c72635d37c3b7c6155320
Gscalmati <gioscalmati@hotmail.com>
2021-11-03 23:37:02 -0300

    Modificacion en ruta de imagenes y preparacion Sprint 5

--- src/routes/products.js
+++ src/routes/products.js
@@ -5,12 +5,12 @@ const fs = require('fs');
 
 /* Configuración de Multer  */
 const multer = require("multer");
-const storage = multer.diskStorage({
+const storageProducts = multer.diskStorage({
     destination : (req, file, cb) =>{               
-        if (!fs.existsSync(path.resolve(__dirname, `../../public/img/${req.body.game_name}-imgs`))){
-            fs.mkdirSync(path.resolve(__dirname, `../../public/img/${req.body.game_name}-imgs`));
+        if (!fs.existsSync(path.resolve(__dirname, `../../public/img/products/${req.body.game_name}-imgs`))){
+            fs.mkdirSync(path.resolve(__dirname, `../../public/img/products/${req.body.game_name}-imgs`));
         }
-        cb(null, path.resolve(__dirname, `../../public/img/${req.body.game_name}-imgs`));
+        cb(null, path.resolve(__dirname, `../../public/img/products/${req.body.game_name}-imgs`));
     },
     filename: (req, file, cb) => {
         let newFilename
@@ -22,13 +22,13 @@ const storage = multer.diskStorage({
         cb(null, newFilename )
     }
 })
-const upload = multer({storage});
+const uploadProduct = multer({storageProducts});
 
 const productsController = require("../controllers/productsController");
 
 
 router.get("/", productsController.categories);
-router.post("/", upload.fields([{name: "logo"},{name: "images"}]), productsController.store);
+router.post("/", uploadProduct.fields([{name: "logo"},{name: "images"}]), productsController.store);
 
 /* CRUD */
 router.get("/dashboard", productsController.dashboard);
@@ -41,7 +41,7 @@ router.get("/edit/:id", productsController.edit);
 
 router.post("/edit", productsController.edit);
 /*Actualizar producto */
-router.put("/edit/:id", upload.fields([{name: "logo"},{name: "images"}]), productsController.update);
+router.put("/edit/:id", uploadProduct.fields([{name: "logo"},{name: "images"}]), productsController.update);
 /* Crear Producto*/
 router.get("/create", productsController.create);
 
