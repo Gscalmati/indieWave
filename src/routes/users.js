811883a26c8e021cd9c50ae34ed87f866142b9d0
Facundo-B <fmblanco97@gmail.com>
2021-11-10 22:13:10 -0300

    merge users.js

--- src/routes/users.js
+++ src/routes/users.js
@@ -17,18 +17,13 @@ const storageUsers = multer.diskStorage({
 const uploadUser = multer({storageUsers});
 const usersController = require("../controllers/usersController");
 
-<<<<<<< HEAD
 /* Vista registro de usuarios */ 
 router.get("/register", usersController.register);
 
 /* Registrar Usuario */
 router.post("/register",uploadUser.single("profile-pic") ,usersController.store);
 
-/*Vista login*/
-router.get("/login", usersController.login);
-=======
 const loggedMiddleware = require("../middlewares/loggedRoutesMiddleware");
-
 
 router.get("/register", loggedMiddleware, usersController.register);
 
@@ -37,6 +32,5 @@ router.get("/login", loggedMiddleware, usersController.login);
 router.post("/login", usersController.logged);
 
 router.get("/logout", usersController.logout);
->>>>>>> 8446b016f852d444c72790bcc5f0266da198562a
 
 module.exports = router;
