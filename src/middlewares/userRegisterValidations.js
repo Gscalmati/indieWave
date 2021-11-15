537e8afeeb6ee144c19560d6cefe7f0942a951c8
Facundo-B <fmblanco97@gmail.com>
2021-11-14 21:00:33 -0300

    defino validaciones de registro

--- src/middlewares/userRegisterValidations.js
+++ src/middlewares/userRegisterValidations.js
@@ -1,11 +1,20 @@
-/* Config Express Validator */
+const path = require ("path");
+const fs = require("fs");
 
-/* Añadir validaciones de registro */
+let jsonUsers = fs.readFileSync(path.resolve(__dirname, '../data/users.json'), 'utf-8');
+let users = JSON.parse(jsonUsers);
+
 const { check } = require("express-validator");
 const validations = [
-    check("username").isEmpty().withMessage("Ingrese un Email o Usuario Válido"),
-    check("password").isEmpty().withMessage("Ingrese su contraseña"),
-    
+    check("username").custom(username => {
+        return users.find(user => user.username == username) != undefined
+    }).withMessage("Ya existe un usuario con este nombre de usuario"),
+    check("email").isEmail().withMessage("Ingrese un email válido").custom(email => {
+        return users.find(user => user.email == email) != undefined
+    }).withMessage("Ya existe un usuario asociado a este email"),,
+    check("name").isAlpha().withMessage("Ingrese un nombre y un apellido válidos"),
+    check("surname").isAlpha().withMessage("Ingrese un nombre y un apellido válidos"),
+    check("password").isLength({min: 8, max: 12}).withMessage("La contraseña debe tener entre 8 y 12 caracteres"),
 ]
 
 module.exports = validations
