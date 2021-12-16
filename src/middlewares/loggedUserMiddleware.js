2fb3356b8917e51f6a502965c3ad731a859d726a
Gscalmati <gioscalmati@hotmail.com>
2021-12-16 19:14:09 -0300

    Actualizo Login con BD nueva, modificando metodo LOGGED, y realizo cambios en HTML para referenciar profile_pic

--- src/middlewares/loggedUserMiddleware.js
+++ src/middlewares/loggedUserMiddleware.js
@@ -1,14 +1,19 @@
+let db = require("../../database/models");
+
 module.exports = function loggedUserMiddleware(req, res, next) {
     res.locals.loggedSession = false;
 
     /*if(req.cookies.usuario){
+        db.Users.findByPK()
         buscar en BD con dato de la cookie y asignarle a req.session.userlogged
         Esto es para hacer un AUTOLOGIN
     }
     */
+
     if (req.session.userLogged) {
         res.locals.loggedSession = true;
         res.locals.userLogged = req.session.userLogged
     }
+
     next()
 }
