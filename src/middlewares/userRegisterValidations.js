9cf433d709dfa25b3a47a1cc66abc25b66c90123
Gscalmati <gioscalmati@hotmail.com>
2021-12-27 22:34:48 -0300

    Actualizo validaciones de back-end del REGISTRO de usuarios

--- src/middlewares/userRegisterValidations.js
+++ src/middlewares/userRegisterValidations.js
@@ -1,10 +1,57 @@
-const path = require("path");
+// Configuracion para JSON Database
+/*const path = require("path");
 const fs = require("fs");
 
 let jsonUsers = fs.readFileSync(path.resolve(__dirname, '../data/users.json'), 'utf-8');
 let users = JSON.parse(jsonUsers);
+*/
 
+const db = require("../../database/models")
 const { body } = require("express-validator");
+
+const validations = [
+    body("name").notEmpty().isLength({ min: 8 }).withMessage("Ingrese un nombre mayor a 2 caracteres")
+    .isAlpha('es-ES', { ignore: '\s' }).withMessage("Ingrese un nombre válido"),
+    body("surname").notEmpty().isLength({ min: 8 }).withMessage("Ingrese un apellido")
+    .isAlpha('es-ES', { ignore: '\s' }).withMessage("Ingrese un apellido válido"),
+    body("email").notEmpty().withMessage("Ingrese un email")
+    .isEmail().withMessage("Ingrese un email con formato válido").bail()
+    .custom((value) => {
+        return db.Users.findOne({
+                where: {
+                    email: value
+                },
+            })
+            .then(result => {
+                if (result) {
+                    return Promise.reject("Ya existe un usuario asociado a este email");
+                }
+                //No poner CATCH porque sobreescribe el "reject" y evita que se cree un error para la vista.
+            })
+
+    }),
+    body("username").notEmpty().isLength({ min: 4 }).withMessage("Ingrese un nombre de usuario mayor a 4 caracteres")
+    .custom((value) => {
+        return db.Users.findOne({
+                where: {
+                    username: value
+                }
+            })
+            .then(function(result) {
+                if (result) {
+                    return Promise.reject("Ya existe un usuario con ese nombre");
+                }
+                //No poner CATCH porque sobreescribe el "reject" y evita que se cree un error para la vista.
+            })
+    }),
+    body("password").notEmpty().withMessage("Ingrese contraseña")
+    .isLength({ min: 8, max: 12 }).withMessage("La contraseña debe tener entre 8 y 12 caracteres"),
+    body("repassword").custom((pass, { req }) => { //Validacion para confirmar constraseña, traigo al "req" con el deconstructor
+        return (pass == req.body.password)
+    }).withMessage("Las contraseñas no coinciden")
+]
+
+/* Validaciones de JSON
 const validations = [
     body("username").custom(username => {
         return users.find(user => user.username == username) == undefined
@@ -12,9 +59,14 @@ const validations = [
     body("email").isEmail().withMessage("Ingrese un email válido").bail().custom(email => {
         return users.find(user => user.email == email) == undefined
     }).withMessage("Ya existe un usuario asociado a este email"),
-    body("name").isAlpha('es-ES', {ignore: '\s'}).withMessage("Ingrese un nombre y un apellido válidos"),
+    body("name").isAlpha('es-ES', { ignore: '\s' }).withMessage("Ingrese un nombre y un apellido válidos"),
     body("surname").isAlpha().withMessage("Ingrese un nombre y un apellido válidos"),
     body("password").isLength({ min: 8, max: 12 }).withMessage("La contraseña debe tener entre 8 y 12 caracteres"),
-]
+    body("repassword").custom((pass, { req }) => { //Validacion para confirmar constraseña, traigo al "req" con el deconstructor
+        return (pass == req.body.password)
+    }).withMessage("Las contraseñas no coinciden")
+]*/
+
+
 
 module.exports = validations
