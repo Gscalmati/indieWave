91ecfb04144c26dabc69b50e3f79d59ebea6127f
Facundo Blanco <facblanc@publicisgroupe.net>
2021-12-30 21:39:11 -0300

    armo middleware de validaciones de Login

--- src/middlewares/userLoginValidations.js
+++ src/middlewares/userLoginValidations.js
@@ -1,2 +1,22 @@
 const db = require("../../database/models");
 const { body } = require("express-validator")
+
+const validations = [
+    body("username").notEmpty().withMessage("Ingrese un email o nombre de usuario")
+        .custom((value) => {
+            return db.Users.findOne({
+                where: {
+                    [Op.or]: [{ username: req.body.username }, { email: req.body.username }]
+                },
+                raw: true
+            })
+                .then(function (result) {
+                    if (result) {
+                        return Promise.reject("No encontramos un usuario con ese nombre/email");
+                    }
+                })
+        }),
+    body("password").notEmpty().withMessage("Ingrese la contraseña")
+]
+
+module.exports = validations
