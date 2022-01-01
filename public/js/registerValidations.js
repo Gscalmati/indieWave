69c4092d04807d15156d63819351d2b5890d6f50
Gscalmati <gioscalmati@hotmail.com>
2021-12-31 21:21:10 -0300

    Modifico validaciones Front de Usuarios Register

--- public/js/registerValidations.js
+++ public/js/registerValidations.js
@@ -1,8 +1,5 @@
 window.onload = function() {
 
-
-
-
     let formulario = document.querySelector("#register-form");
 
     formulario.addEventListener("submit", function(event) {
@@ -12,48 +9,51 @@ window.onload = function() {
         let fieldName = document.querySelector("input#name");
 
         if (fieldName.value.length < 2) {
-            errores.push({ param: "name", msg: "Ingrese un nombre mayor a 2 caracteres" })
+            errores.push({ param: "name", msg: "FRONT Ingrese un nombre mayor a 2 caracteres" })
         }
 
         let fieldSurname = document.querySelector("input#surname");
 
         if (fieldSurname.value.length < 2) {
-            errores.push({ param: "surname", msg: "Ingrese un apellido mayor a 2 caracteres" })
+            errores.push({ param: "surname", msg: "FRONT Ingrese un apellido mayor a 2 caracteres" })
         }
 
         let fieldEmail = document.querySelector("input#email");
 
-        if (fieldEmail.value.length < 2) {
-            errores.push({ param: "email", msg: "Ingrese un apellido mayor a 2 caracteres" })
+        if (fieldEmail.value.length == "") {
+            errores.push({ param: "email", msg: "FRONT Ingrese un email" })
         }
 
         let fieldUsername = document.querySelector("input#username");
 
         if (fieldUsername.value == "") {
-            errores.push({ param: "username", msg: "El campo no puede estar vacío" })
+            errores.push({ param: "username", msg: "FRONT El campo no puede estar vacío" })
         } else if (fieldUsername.value.length < 4) {
-            errores.push({ param: "username", msg: "El username debe tener al menos 4 caracteres" })
+            errores.push({ param: "username", msg: "FRONT El username debe tener al menos 4 caracteres" })
         }
 
         let fieldPassword = document.querySelector("input#password");
 
         if (fieldPassword.value.length < 8) {
-            errores.push({ param: "password", msg: "La contraseña debe tener al menos 8 caracteres" })
+            errores.push({ param: "password", msg: "FRONT La contraseña debe tener al menos 8 caracteres" })
         }
 
         let fieldRepassword = document.querySelector("input#repassword");
 
-        if (fieldRepassword.value != fieldPassword.value) {
-            errores.push({ param: "repassword", msg: "Ambas contraseñas deben coincidir" })
+        if (fieldRepassword.value == "") {
+            errores.push({ param: "repassword", msg: "FRONT Confirme contraseña" })
+        } else if (fieldRepassword.value != fieldPassword.value) {
+            errores.push({ param: "repassword", msg: "FRONT Ambas contraseñas deben coincidir" })
         }
 
         console.log(errores)
-        let erroresUL = document.querySelector("#errors-ul")
+            //let erroresUL = document.querySelector("#errors-ul")
         if (errores.length > 0) {
             event.preventDefault()
             errores.forEach(elemento => {
-                erroresUL.innerHTML += "<li>" + "<i class='fas fa-exclamation-circle'></i>" + elemento.msg + "</li>"
-
+                let error = document.querySelector("#error-" + elemento.param);
+                error.innerHTML += "<span>" + "<i class='fas fa-exclamation-circle'></i>" + elemento.msg + "</span>";
+                //erroresUL.innerHTML += "<li>" + "<i class='fas fa-exclamation-circle'></i>" + elemento.msg + "</li>"
             })
         }
 
