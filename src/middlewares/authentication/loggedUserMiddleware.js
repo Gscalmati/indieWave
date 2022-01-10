5b369a0a5cba235a0936feca296ff58964ce9881
Gscalmati <gioscalmati@hotmail.com>
2022-01-10 00:16:12 -0300

    Modifico metodo Saveprofile para validaciones, ARREGLO AUTOLOGIN

--- src/middlewares/authentication/loggedUserMiddleware.js
+++ src/middlewares/authentication/loggedUserMiddleware.js
@@ -9,12 +9,20 @@ module.exports = function loggedUserMiddleware(req, res, next) {
         if (req.session.userLogged == undefined && (req.cookies != undefined && req.cookies.email != undefined)) {
             res.locals.loggedSession = true;
 
-            res.locals.userLogged = await db.Users.findOne({
+            req.session.userLogged =  await db.Users.findOne({
                 where: {
                     email: req.cookies.email
                 }
             })
+
+            /*res.locals.userLogged = await db.Users.findOne({
+                where: {
+                    email: req.cookies.email
+                }
+            })*/ 
+            //ESTO SE SOLUCIONA LUEGO EN EL OTRO IF...SOLO TENIA QUE ASIGNAR LA SESSION CON EL USUARIO DE LA COOKIE
         }
+          
 
         if (req.session.userLogged) {
 
