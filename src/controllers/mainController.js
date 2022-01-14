e1919f42e5738f191365892b3f32969eb38f2117
bottassiJuanIgnacio <bottassi2012@gmail.com>
2022-01-13 22:50:17 -0300

    productos home clickeables

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,9 +1,33 @@
 const path = require('path');
-
+const db = require("../database/models");
 let mainController = {
     home: (req,res) => {
-        res.render("main/home");
+
+
+
+        db.Products.findAll({ limit: 4 , raw:true})
+        .then(gameFeatured => {
+           return res.render("main/home",{productsFeatured: gameFeatured});
+        })
+        .catch(error =>{
+            console.log(error);
+        })
+
+
+        db.Products.findAll({ limit: 5 , raw:true})
+        .then(gameFresh =>{
+            return res.render("main/home", {productsFresh: gameFresh});
+        })
+        .catch(error =>{
+            console.log(error);
+        })
+
+
+
+        
     },
 }
 
 module.exports = mainController;
+
+
