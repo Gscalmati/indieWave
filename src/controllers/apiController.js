b4a2df915c022611903cc315b6b6f3a3ab89a999
Facundo-B <fmblanco97@gmail.com>
2022-01-23 14:41:36 -0300

    añado método de listado de usuarios con paginado

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -1,14 +1,50 @@
 const path = require('path');
 const db = require("../database/models");
-let mainController = {
-   
-    getUsers: async (req,res) => {
-        res.send("Estoy vivo");
+const sequelize = require("sequelize");
+
+
+let apiController = {
+
+    getUsers: async (req, res) => {
+
+        let userCount = await db.Users.count();
+        let lastPage = Math.floor(userCount / 10);
+
+        console.log("userCount : " + userCount) 
+        console.log("lastPage:" + lastPage)
+
+
+        if ( !(req.query.page == undefined || req.query.page == "" || isNaN(req.query.page)) ) {
+            page = parseInt(req.query.page)
+        } else {
+            res.redirect("/api/users?page=0")
+        }
+
+        if( page > lastPage){
+            res.redirect(`/api/users?page=${lastPage}`)
+        }
+        
+        let users = await db.Users.findAll({
+            attributes: ["id", "email", "username", "name", "surname"],
+            order: [['id', 'ASC']],
+            limit: 10,
+            offset: page * 10,
+        })
+        
+        console.log(req.query)
+        console.log("page:" + page)
+
+        res.json({
+            prev: page > 0 ? `localhost:3000/api/users?page=${page - 1}` : null,
+            count: userCount,
+            users: users,
+            next: page != lastPage ? `localhost:3000/api/users?page=${page + 1}` : null
+        });
     }
 }
 
 
 
-module.exports = mainController;
+module.exports = apiController;
 
 
