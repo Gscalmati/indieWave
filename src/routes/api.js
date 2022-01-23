9211401f6bf35b64206266618bcbaf1a9667b8c4
Facundo-B <fmblanco97@gmail.com>
2022-01-23 16:35:39 -0300

    añado búsqueda por id a la Api de usuarios

--- src/routes/api.js
+++ src/routes/api.js
@@ -6,4 +6,7 @@ const apiController = require("../controllers/apiController");
 /*Listado de usuarios */
 router.get("/users", apiController.getUsers);
 
+/*Obtener usuario por id */
+router.get("/users/:id", apiController.getUserById);
+
 module.exports = router;
