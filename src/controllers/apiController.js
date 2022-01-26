2580e6148503d0f4f1a0e7bb129bedf717191854
bottassiJuanIgnacio <bottassi2012@gmail.com>
2022-01-26 13:55:29 -0300

    api products y juegos destacados imgs

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -67,9 +67,31 @@ let apiController = {
     
     getProducts: async (req, res) => {
         try {
-            let products = await db.Products.findAll( {attributes: ['id', 'name', 'description','genre_id'] });
+            let productCount = await db.Products.count();
+            let lastPage = Math.floor(productCount / 10);
+
+
+            if (!(req.query.page == undefined || req.query.page == "" || isNaN(req.query.page) || req.query.page < 0)) {
+                page = parseInt(req.query.page)
+            } else {
+                res.redirect("/api/products?page=0")
+            }
+
+            if (page > lastPage) {
+                res.redirect(`/api/products?page=${lastPage}`)
+            }
+
+            let products = await db.Products.findAll( {
+                attributes: ['id', 'name', 'description','genre_id'],
+                order: [['id', 'ASC']],
+                limit: 10, //no toma limit 10
+                offset: page * 10,
+                raw: true });
             
-            let productsImages = await db.Images.findAll( {include : ["products"]});
+            let productsImages = await db.Images.findAll( {
+                include : ["products"],
+                limit:10
+            });
             
             let productsByGenre = {}
             let genres = await db.Genres.findAll();
@@ -91,12 +113,13 @@ let apiController = {
             })
             
 
-            
             res.json({
-                count: products.length,
+                prev: page > 0 ? `localhost:3000/api/products?page=${page - 1}` : null,
+                count: productCount,
                 data:products,
                 images: productsImages,
-                countByGenre:productsByGenre
+                countByGenre:productsByGenre,
+                next: page != lastPage ? `localhost:3000/api/products?page=${page + 1}` : null
             })
         }
         catch (error) {
@@ -107,23 +130,21 @@ let apiController = {
 
     getProductById:  async (req, res) => {
         try {
+            /*Detalle de un producto */
             let product = await db.Products.findByPk(req.params.id);
 
             /*un array de uno a muchos */
-            let productsGenre = await db.Products.findByPk(req.params.id, {include: [{ association: "genre" }]});
+            //,options = { attributes: { exclude: ['id', 'name','developer','email', 'release_date','price','logo','min_requirements','rec_requirements','description'] } }
+            let productImages = await db.Products.findByPk(req.params.id, {include: [{ association: "images" }]});
             
-            // let productImage = await db.Products.findAll();
-            // productImage.forEach(product => {
-            //     let images = product.images.map(image =>{
-            //         return 'http://localhost:3000/public/img/products/' + images.url
-            //     })
-            // });
-            // product.images = images;
-
+            /*URL del logo */
+            /*Se imprime la URL en la api pero no encuentra la ruta*/
+            console.log(product.logo);
+            let productUrl = `localhost:3000/public${product.logo}`
+            
             res.json({
-                data:product,
-                genre:productsGenre,
-                //images: img
+                data:productImages,
+                url: productUrl
             })
         }
         catch (error) {
