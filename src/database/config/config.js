67a0cca0b976484d42527d2703bc3e0861857712
bottassiJuanIgnacio <bottassi2012@gmail.com>
2022-02-04 01:44:38 -0300

    animaciones home y db products

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,21 +2,21 @@ module.exports = {
     "development": {
         "username": "root",
         "password": null,
-        "database": "indiewave_db",
+        "database": "indiewave",
         "host": "127.0.0.1",
         "dialect": "mysql"
     },
     "test": {
         "username": "root",
         "password": null,
-        "database": "indiewave_db",
+        "database": "indiewave",
         "host": "127.0.0.1",
         "dialect": "mysql"
     },
     "production": {
         "username": "root",
         "password": null,
-        "database": "indiewave_db",
+        "database": "indiewave",
         "host": "127.0.0.1",
         "dialect": "mysql"
     }
