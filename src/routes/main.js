187f609462364c0810ae827afbdc5f6102759535
Gio Scalmati <gioscalmati@hotmail.com>
2025-06-10 22:02:48 -0300

    saco url temp

--- src/routes/main.js
+++ src/routes/main.js
@@ -5,10 +5,6 @@ const mainController = require("../controllers/mainController");
 
 
 router.get("/", mainController.home);
-router.get('/ip', async (req, res) => {
-    const ip = await fetch('https://api.ipify.org?format=json').then(r => r.json());
-    res.send(`IP pública detectada desde Render: ${ip.ip}`);
-  });
 router.get("/cordoba", mainController.cordoba);
 
 module.exports = router;
