6222689678c3fcdba99970e8b0520f9b15df8ae3
Gio Scalmati <gioscalmati@hotmail.com>
2025-06-10 21:32:47 -0300

    Nueva config de bd

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -3,21 +3,21 @@ module.exports = {
         "username": "u809776786_root",
         "password": "Indiewave123.",
         "database": "u809776786_indiewave",
-        "host": "gioscalmati.com.ar",
+        "host": "srv725.hstgr.io",
         "dialect": "mysql"
     },
     "test": {
         "username": "u809776786_root",
         "password": "Indiewave123.",
         "database": "u809776786_indiewave",
-        "host": "gioscalmati.com.ar",
+        "host": "srv725.hstgr.io",
         "dialect": "mysql"
     },
     "production": {
         "username": "u809776786_root",
         "password": "Indiewave123.",
         "database": "u809776786_indiewave",
-        "host": "gioscalmati.com.ar",
+        "host": "srv725.hstgr.io",
         "dialect": "mysql"
     }
 }
